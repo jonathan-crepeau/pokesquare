46a1d30ede76249a0218efa4b1c92499240661d5
jonathan-crepeau <jon.crepeau@gmail.com>
2022-08-26 15:55:47 -0700

    game start & cube color functions complete

--- js/updatedapp.js
+++ js/updatedapp.js
@@ -1,1 +1,30 @@
 // console.log('Fear is the mind killer.');
+
+let score = 0;
+
+const btn = document.querySelector('.start-button');
+
+btn.addEventListener('click', () => {
+    console.log('Clicked!');
+    playBall(12);
+});
+
+function playBall(numberOfSquares) {
+    for (let i = 0; i < numberOfSquares; i++) {
+        const square = $('<div class="game-square"></div>');
+        console.log('Added!');
+        assignColor(square);
+        $('.squares-container').append(square);
+    }
+}
+
+function assignColor(element) {
+    const number = Math.floor(Math.random() * (3 - 1 + 1) + 1);
+    if (number === 1) {
+        $(element).css('background-color', 'red');
+    } else if (number === 2) {
+        $(element).css('background-color', 'blue');
+    } else {
+        $(element).css('background-color', 'green');
+    }
+}
